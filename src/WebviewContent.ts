1d68503506c3db9d5d65d6d9015b6cab6ca0d85b
Damien Sedgwick <damiensedgwick@icloud.com>
2021-08-20 12:27:11 +0100

    WIP: Trying to get compiled app to load.

--- src/WebviewContent.ts
+++ src/WebviewContent.ts
@@ -1,44 +1,31 @@
-export const getWebviewContent = () => {
-  return `
-          <!DOCTYPE html>
-          <html lang="en">
-              <head>
-                  <meta charset="UTF-8">
-                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                  <title>XState Visualizer</title>
-                  <style>
-                  
-                  * {
-                      
-                  }
-                  
-                  body {
-                      padding: 0;
-                      margin: 0;
-                      box-sizing: border-box;
-                      height: calc(100vh - 5px);
-                      width: calc(100vw - 10px);
-                      margin: auto;
-                  }
-                  
-                  .iframe-wrapper {
-                      height: 100%;
-                      width: 100%;
-                  }
+import * as vscode from "vscode";
+import * as path from "path";
 
-                  .iframe {
-                      height: 100%;
-                      width: 100%;
-                  }
-                  
-                  </style>
-              </head>
-              
-              <body>
-                  <div class="iframe-wrapper">
-                      <iframe class="iframe" src="https://stately.ai/viz" title="XState Visualizer Iframe">
-                  </div>
-              </body>
-          </html>
+export const getWebviewContent = () => {
+  const reactAppPathOnDisk = vscode.Uri.file(
+    path.join(vscode.env.appRoot, "app", "src/app.js")
+  );
+
+  const reactAppUri = reactAppPathOnDisk.with({ scheme: "vscode-resource" });
+
+  return `
+            <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>XState Visualizer</title>
+
+                    <script>
+                        window.acquireVsCodeApi = acquireVsCodeApi;
+                    </script>
+                </head>
+
+                <body>
+                    <div id="root"></div>
+
+                    <script src="${reactAppUri}"></script>
+                </body>
+            </html>
       `;
 };
